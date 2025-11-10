5fe04bf8a03fa667cc8c81608cfcff15e4eb1046
Júlio <125740363+Junlys3@users.noreply.github.com>
2025-11-09 21:49:32 -0300

    corecao

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new resource...
      */
     public function create()
     {
